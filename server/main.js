f7fb54589bae555983c5cd0dcd1567bb8d291fec
technobaba418 <113593828+technobaba418@users.noreply.github.com>
2022-09-16 07:48:18 +0000

    Update main.js

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const WebSocket = require('ws');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
