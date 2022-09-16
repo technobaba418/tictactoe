54b2e9b3de2620905832370b246de9dfd14e9976
technobaba418 <113593828+technobaba418@users.noreply.github.com>
2022-09-16 07:35:44 +0000

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env browser */
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game';
-import './index.css';
-
-ReactDOM.render(<Game />, document.getElementById('root'));
